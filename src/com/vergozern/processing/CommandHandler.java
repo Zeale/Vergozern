898858ff3b97c59342fa1903536298e1e5f89197
Zeale <zeale@dusttoash.org>
2019-03-28 17:13:48 -0500

    Added a command handler

---
+++ src/com/vergozern/processing/CommandHandler.java
@@ -0,0 +1,32 @@
+package com.vergozern.processing;
+
+import org.alixia.javalibrary.commands.GenericCommandManager;
+import org.alixia.javalibrary.commands.QuickGenericCommand;
+import org.alixia.javalibrary.strings.matching.ManipulableString;
+import org.alixia.javalibrary.strings.matching.Matching;
+
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+
+public final class CommandHandler {
+	private CommandHandler() {
+	}
+
+	private final GenericCommandManager<MessageReceivedEvent> commandManager = new GenericCommandManager<>();
+
+	private abstract class Command implements QuickGenericCommand<MessageReceivedEvent> {
+		{
+			commandManager.addCommand(this);
+		}
+	}
+
+	private abstract class StringCommand extends Command {
+		@Override
+		public boolean match(MessageReceivedEvent data) {
+			return new ManipulableString(data.getMessage().getContent()).consumeIf(Matching.build("~")
+					.or(Matching.ignoreCase(data.getClient().getApplicationName()).possibly(",").then(" "))) != null
+							? super.match(data)
+							: false;
+		}
+	}
+
+}
