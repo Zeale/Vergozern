2b2b8c962e8d1a6fa6fdbbaebfff1b4ef4343da0
Zeale <zeale@dusttoash.org>
2019-03-28 16:29:20 -0500

    Added a launch class

---
+++ src/com/vergozern/launch/Launch.java
@@ -0,0 +1,10 @@
+package com.vergozern.launch;
+
+import sx.blah.discord.api.ClientBuilder;
+
+public class Launch {
+	public static void main(String[] args) {
+		ClientBuilder builder = new ClientBuilder()
+				.withToken("NTYwNDQ3MDg2NTE3Mjg4OTgw.D33R6g.2yxvykkFpF-ocw-ECyyM9Mmmnwo");
+	}
+}
