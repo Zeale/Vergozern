453cd3c3daaea32427c3c57a30c660034f1ba5bf
Zeale <zeale@dusttoash.org>
2019-04-02 23:53:03 -0500

    I'm going to have to reset my bot token.

--- src/com/vergozern/launch/Launch.java
+++ src/com/vergozern/launch/Launch.java
@@ -1,10 +1,12 @@
 package com.vergozern.launch;
 
+import com.vergozern.processing.CommandHandler;
+
 import sx.blah.discord.api.ClientBuilder;
 
 public class Launch {
 	public static void main(String[] args) {
-		ClientBuilder builder = new ClientBuilder()
-				.withToken("NTYwNDQ3MDg2NTE3Mjg4OTgw.D33R6g.2yxvykkFpF-ocw-ECyyM9Mmmnwo");
+		new ClientBuilder().withToken("NTYwNDQ3MDg2NTE3Mjg4OTgw.D33R6g.2yxvykkFpF-ocw-ECyyM9Mmmnwo")
+				.registerListener(new CommandHandler()).build();
 	}
 }
